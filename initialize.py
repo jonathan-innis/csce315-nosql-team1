2347effa300c5284bcb2243a81a9cde3897f4b5f
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-11 21:02:11 -0500

    Updated the skeleton files

--- initialize.py
+++ initialize.py
@@ -16,19 +16,22 @@ def get_connection():
         print "Acquiring connection to Mongo..."
         client = MongoClient('localhost', 27017)
         db = client['movies_mongo']
-        print "Importing movies..."
-        movies_collection = db['movies']
-        movies_collection.insert(get_movies_data())
-        print "Importing credits..."
-        credits_collection = db['credits']
-        credits_collection.insert(get_credits_data())
-        print "Done"
+        return db
     
     except Exception as ex:
         print ex
 
 def main():
-    get_connection()
+    db = get_connection()
+    print "Importing movies..."
+    movies_collection = db['movies']
+    movies_collection.insert(get_movies_data())
+    print "Importing credits..."
+
+    #TODO Import credits collection according to spec
+    #credits_collection = db['credits']
+    #credits_collection.insert(get_credits_data())
+    print "Done"
 
 if __name__ == '__main__':
     main()
---
+++ main.py
@@ -0,0 +1,19 @@
+from initialize import get_connection
+
+def getMoviesByMovieId(movie_id):
+    return
+
+def getRecordByIMDBId(imdb_id):
+    return
+
+def getMovieStats():
+    return
+
+def getCaseByMovieId():
+    return
+
+def getCreditsStats():
+    return
+
+def getAggregateRecordByMovieId(movie_id):
+    return
