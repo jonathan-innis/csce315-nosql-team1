064901775f4412148a0e6979e2b9b8f2dc7dbdf0
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-11 20:51:00 -0500

    Have a connection to mongo with filees imported

--- initialize.py
+++ initialize.py
@@ -1,9 +1,34 @@
 import json
+from pymongo import MongoClient
 
-with open('data/credits.json') as json_data:
-    credits = json.load(json_data)
-    print credits
+def get_credits_data():
+    with open('data/credits.json') as json_data:
+        credits = json.load(json_data)
+        return credits
 
-with open('data/movies.json') as json_data:
-    movies = json.load(json_data)
-    print movies
+def get_movies_data():
+    with open('data/movies.json') as json_data:
+        movies = json.load(json_data)
+        return movies
+
+def get_connection():
+    try:
+        print "Acquiring connection to Mongo..."
+        client = MongoClient('localhost', 27017)
+        db = client['movies_mongo']
+        print "Importing movies..."
+        movies_collection = db['movies']
+        movies_collection.insert(get_movies_data())
+        print "Importing credits..."
+        credits_collection = db['credits']
+        credits_collection.insert(get_credits_data())
+        print "Done"
+    
+    except Exception as ex:
+        print ex
+
+def main():
+    get_connection()
+
+if __name__ == '__main__':
+    main()
