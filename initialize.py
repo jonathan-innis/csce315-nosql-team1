d092260e11fac91a27b939e31bfe674bf503af76
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-13 17:54:55 -0500

    Updated the merge function

--- initialize.py
+++ initialize.py
@@ -21,42 +21,45 @@ def get_movies_data():
         movies = json.load(json_data)
         return movies
 
-def main():
-    with MongoConnection(COLLECTION, DB_ENDPOINT) as db:
-        ############################
-        #print "Importing movies..."
-        movies_collection = db['movies']
-        #movies_collection.insert(get_movies_data())
-        ############################
-        print "Importing credits..."
-        credits = get_credits_data()
-        movies_collection.create_index(
-            [
-                ('id', pymongo.ASCENDING),
-                ('credits.id', pymongo.DESCENDING)
-            ]
-        )
+def add_movies_collection(db):
+    print "Importing movies..."
+    movies = get_movies_data()
+    movies_collection = db['movies']
+    movies_collection.insert(movies)
+    movies_collection.create_index(
+        [
+            ('id', pymongo.ASCENDING),
+            ('credits.id', pymongo.DESCENDING)
+        ]
+    )
 
-        for credit in credits:
-            creditID = int(credit['id'])
+def merge_credits_collection(db):
+    print "Merging credits into movies..."
+    movies_collection = db['movies']
+    credits = get_credits_data()
+    for credit in credits:
+        creditID = int(credit['id'])
 
-            movies_collection.update(
-                {
-                    u'id' : creditID
-                },
-                {
-                    "$set" : {
-                        u'credits' : credit['cast']
+        movies_collection.update(
+            {
+                u'id' : creditID
+            },
+            {
+                "$set" : {
+                    u'credits' : {
+                        u'cast' : credit['cast'],
+                        u'crew' : credit['crew']
                     }
                 }
-            )
-            print("updated:{id}".format(id=creditID))
+            }
+        )
+        print("updated:{id}".format(id=creditID))
 
-        """
-        #TODO Clean up this function of any bad practices
-        #TODO Get it to connect to the mongo server consistently
-        """
-        print "Done"
+def main():
+    with MongoConnection(COLLECTION, DB_ENDPOINT) as db:
+        add_movies_collection(db)
+        merge_credits_collection(db)
+
 
 if __name__ == '__main__':
     main()
