2eda281db5a2fef66aa14fdb941d1741741130ba
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-16 10:33:49 -0500

    Commented the movies document up

--- movies.py
+++ movies.py
@@ -12,6 +12,14 @@ DB_ENDPOINT = 'mongodb://{username}:{password}@13.58.47.75:27017/movies_mongo'.f
 )
 
 def getMoviesByMovieId(movie_id):
+    """Takes in a movie_id and returns the corresponding movie that is associated with it
+    
+    Arguments:
+        movie_id: unique integer identifier for a movie
+    Assumptions:
+        Assumes the movie collection has already been initialized
+    """
+
     with MongoConnection(COLLECTION, DB_ENDPOINT) as db:
         movie_collection = db['movies']
         ret = movie_collection.find_one({'id': movie_id})
@@ -32,16 +40,27 @@ def getCreditsStats():
     return
 
 def getPersonById(person_id):
+    """Takes in a person_id and returns the corresponding person that is associated with it
+    
+    Arguments:
+        person_id: unique integer identifier for a person
+    Assumptions:
+        Assumes the person and movie collection have already been initialized
+    """
+
     with MongoConnection(COLLECTION, DB_ENDPOINT) as db:
         people_collection = db['people']
         movie_collection = db['movies']
         person = people_collection.find_one({'id': person_id})
+
+        #Replaces the cast_in array with the actual movies associated with the ids
         if 'cast_in' in person:
             cast_in = []
             for movie_id in person['cast_in']:
                 cast_in.append(movie_collection.find_one({'_id': movie_id}))
             person['cast_in'] = cast_in
         
+        #Replaces the crew_in array with the actual movies associated with the ids
         if 'crew_in' in person:
             crew_in = []
             for movie_id in person['crew_in']:
@@ -51,6 +70,14 @@ def getPersonById(person_id):
         return dumps(person)
 
 def getAggregateRecordByMovieId(movie_id):
+    """Takes in a movie_id and returns the corresponding movie that is associated with it
+    
+    Arguments:
+        movie_id: unique integer identifier for a movie
+    Assumptions:
+        Assumes the movie collection has already been initialized
+    """
+
     with MongoConnection(COLLECTION, DB_ENDPOINT) as db:
         movie_collection = db['movies']
         ret = movie_collection.find_one({'id': movie_id})
