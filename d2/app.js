25ff8c54b186029ee0becec4c0bff55babf80ed9
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-18 12:21:24 -0500

    Updated the app

--- d2/app.js
+++ d2/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const config = require('./config')
 const app = express()
+const port = 3000
 const mongoose = require('mongoose');
+const { db } = config;
 const connectionString = `mongodb://${db.host}:${db.port}/${db.name}`;
 mongoose.connect(connectionString)
 
