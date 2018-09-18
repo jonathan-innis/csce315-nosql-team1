4610c9b91599dd53bb072de63f435da6252cc429
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-18 12:20:05 -0500

    Updated the config

--- d2/app.js
+++ d2/app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const config = require('./config')
 const app = express()
 const mongoose = require('mongoose');
-const { db: { host, port, name } } = config;
-const connectionString = `mongodb://${host}:${port}/${name}`;
+const connectionString = `mongodb://${db.host}:${db.port}/${db.name}`;
 mongoose.connect(connectionString)
 
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${app.port}!`))
