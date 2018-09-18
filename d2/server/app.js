f3adabe63c73323e955faad58c897af8fd31de07
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-18 13:19:47 -0500

    Debug

--- d2/server/app.js
+++ d2/server/app.js
@@ -11,4 +11,4 @@ mongoose.connect(connectionString, {useNewUrlParser: true})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${app.port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
