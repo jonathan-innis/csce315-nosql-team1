a3b1ecf16aa3c456a1377030f272bd7220f6c48a
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-19 14:05:14 -0500

    Updated the naming convention

--- d2/server/mongo-utils.js
+++ d2/server/mongo-utils.js
@@ -4,14 +4,6 @@ const config = require('./config');
 const { db } = config;
 const connectionString = `mongodb://${db.host}:${db.port}/${db.name}`;
 
-async function insertMovieData(){
-    
-}
-
-async function insertPeopleData(){
-
-}
-
 module.exports = {
     getDb: async () => {
         const db = await mongoose.connect(connectionString, {useNewUrlParser: true});
