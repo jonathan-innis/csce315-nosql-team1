c24009ac322129da8a70114ff9f39c4945ff110d
Benton Guess <bguess10@tamu.edu>
2018-09-19 21:44:01 -0500

    Added endpoints for serving static files

--- d2/server/routes/dbservice/index.js
+++ d2/server/routes/dbservice/index.js
@@ -8,11 +8,12 @@ const search = require('./search');
 router.get('/movie', movie);
 router.get('/person', person);
 router.get('/search', search);
+
 router.get(/\/[A-Za-z_0-9]+\.[a-z]+/g, (req, res) =>
     console.log(req.url)//require('../../dist' + req.originalUrl)
 )
 router.get('/', (req, res) =>
-    require("../../home.html")
+    express.static(__dirname  + '../../public/hello.html')
 )
 
 module.exports = router;
