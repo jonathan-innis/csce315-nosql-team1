b9a02b748cf6db6597d4b49c35b5e2d6ef4eff9f
Benton Guess <bguess10@tamu.edu>
2018-09-19 21:27:51 -0500

    Webpack configuration

--- d2/server/routes/dbservice/index.js
+++ d2/server/routes/dbservice/index.js
@@ -8,5 +8,11 @@ const search = require('./search');
 router.get('/movie', movie);
 router.get('/person', person);
 router.get('/search', search);
+router.get(/\/[A-Za-z_0-9]+\.[a-z]+/g, (req, res) =>
+    console.log(req.url)//require('../../dist' + req.originalUrl)
+)
+router.get('/', (req, res) =>
+    require("../../home.html")
+)
 
 module.exports = router;
