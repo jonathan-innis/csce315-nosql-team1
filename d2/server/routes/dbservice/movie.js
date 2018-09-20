b043a22545386899bfcf4c672844ea2d18463190
Joshua McAfee <joshua_mcafee@main-10-230-53-72.tamulink.tamu.edu>
2018-09-20 12:00:02 -0500

    completed /dbservice/movie?movie_id=int and /dbservice/person?person_id=int

--- d2/server/routes/dbservice/movie.js
+++ d2/server/routes/dbservice/movie.js
@@ -3,8 +3,8 @@ const {Movie} = require('../../models');
 module.exports = async (req, res) => {
     try{
         const {movie_id} = req.query;
-        movies = await Movie.findOne({id: movie_id});
-        res.json({movies: movies})
+        const movie = await Movie.findOne({id: movie_id});
+        res.json({movie: movie})
     }
     catch (error){
         res.sendStatus(400);
