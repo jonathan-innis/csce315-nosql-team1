3c58fa955239e407663f260f5c3195295a6b6750
Joshua McAfee <joshua_mcafee@main-10-230-53-72.tamulink.tamu.edu>
2018-09-20 11:53:04 -0500

    completed /dbservice/movie?movie_id=int and /dbservice/person?person_id=int

--- d2/server/app.js
+++ d2/server/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getDb} = require('./initDB');
+const {getDb} = require('./mongo-utils');
 const app = express()
 const port = 3000
 
--- d2/server/routes/dbservice/person.js
+++ d2/server/routes/dbservice/person.js
@@ -1,6 +1,40 @@
-const {Person} = require('../../models');
+const {People} = require('../../models');
+const {Movie}  = require('../../models');
+
+async function convertIdToMovie(_id) {
+    return await Movie.findById(_id);
+}
 
 module.exports = async (req, res) => {
-    res.sendStatus(200);
-    console.log("Hello")
+    try{
+        const {person_id} = req.query;
+        people = [];
+
+        people = await People.find({id: person_id});
+
+        if (people.length > 0){
+            for (let i in people){
+                const {crew_in, cast_in} = people[i];
+                let crewInMovies = [];
+                let castInMovies = [];
+
+                for (let movieId of crew_in){
+                    const movie = await convertIdToMovie(movieId);
+                    crewInMovies.push(movie);
+                }
+                for (let movieId of cast_in){
+                    const movie = await convertIdToMovie(movieId);
+                    castInMovies.push(movie);
+                }
+                people[i]['crew_in'] = crewInMovies;
+                people[i]['cast_in'] = castInMovies;
+            }
+        }
+
+        res.json({people: people})
+    }
+    catch(error){
+        console.log(error)
+        res.sendStatus(400);
+    }
 }
