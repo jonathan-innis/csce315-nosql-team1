c8340c65433069852b736007ceab8822cbdba574
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-09-22 17:34:38 -0500

    Updated the search to be better

--- d2/src/models/People.js
+++ d2/src/models/People.js
@@ -2,7 +2,7 @@ const {people} = require('../schemas');
 const mongoose = require('mongoose');
 
 people.statics.search = async function search(query){
-    return await this.model('People', people, 'people').find({name: { $regex: `(?i).*${query}.*` }}); 
+    return await this.model('People', people, 'people').find({name: { $regex: `(?i)^${query}.*` }}); 
 }
 
 let People = mongoose.model('People', people, 'people');
