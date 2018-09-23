26b5dd62e6eff8a63b5e4c75946c839fc1d74d33
mitcheld <mitcheld@tamu.edu>
2018-09-23 18:12:35 -0500

    tweak front end

--- d2/src/src/jsx/Person.jsx
+++ d2/src/src/jsx/Person.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../css/master.css'
+import {MovieCard} from './Cards.jsx';
+
 
 const queryMatcher = RegExp(/person_id=([0-9]+)/g)
 
@@ -71,11 +73,17 @@ class Person extends React.Component {
                 ) 
             )
 
-            // let movie = this.state.person_data.cast_in.map(
-            //     (val, num) => (
-            //         <MovieCard title={val.movie.title} imglink={val.poster_path} id={val.id} key={num}/>
-            //     )  
-            // )
+            let CastMovie = this.state.person_data.cast_in.map(
+                (val, num) => (
+                    <MovieCard title={val.movie.title} job={val.character} imglink={val.movie.poster_path} id={val.movie.id} key={num}/>
+                )  
+            )
+
+            let CrewMovie = this.state.person_data.crew_in.map(
+                (val, num) => (
+                    <MovieCard title={val.movie.title} job={val.job} imglink={val.movie.poster_path} id={val.movie.id} key={num}/>
+                )  
+            )
 
             console.log(query)
 
@@ -101,6 +109,17 @@ class Person extends React.Component {
                     </div>
                     <div>
 
+                    </div>
+
+                    <div style={{justifyContent: "center",display: "flex", flexDirection: "row" }}>
+                        <div className="cards">
+                            <span className="highlight"> Movies crew in: </span>
+                            {CrewMovie}
+                        </div>
+                        <div className="cards">
+                            <span className="highlight"> Movies cast in: </span>
+                            {CastMovie}
+                        </div>
                     </div>
                 </div>
             )
